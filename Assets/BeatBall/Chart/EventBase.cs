31d216ecd0b185499e515f83d78c8d37cfa01725
Xeltica <citringo@gmail.com>
2018-03-04 14:30:26 +0900

    Event のコンストラクター追加

--- Assets/BeatBall/Chart/EventBase.cs
+++ Assets/BeatBall/Chart/EventBase.cs
@@ -3,15 +3,27 @@
 	public abstract class EventBase
 	{
 		public int Measure { get; set; }
+
+		protected EventBase(int measure) { Measure = measure; }
 	}
 
 	public class BeatEvent : EventBase
 	{
+		public BeatEvent(int measure, Beat beat) : base(measure)
+		{
+			Beat = beat;
+		}
+
 		public Beat Beat { get; set; }
 	}
 
 	public class TempoEvent : EventBase
 	{
+		public TempoEvent(int measure, float tempo) : base(measure)
+		{
+			Tempo = tempo;
+		}
+
 		public float Tempo { get; set; }
 	}
 }
